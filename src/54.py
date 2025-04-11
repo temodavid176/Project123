a3393e5144e25a74bc41b708fd9e68e806e5af86
temodavid176 <temodavid176@gmail.com>
2025-04-11 14:13:33 +0000

    Add 54.py

---
+++ src/54.py
@@ -0,0 +1,18 @@
+def add(x, y):
+    """Add two numbers."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract two numbers."""
+    return x - y
+
+def multiply(x, y):
+    """Multiply two numbers."""
+    return x * y
+
+def divide(x, y):
+    """Divide two numbers."""
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    else:
+        return x / y
